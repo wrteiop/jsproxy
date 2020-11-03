fea7a90ed0e8146181a0fac17f2090564eb4ec1b
YidaozhanYa <73830635+YidaozhanYa@users.noreply.github.com>
2020-11-03 20:18:41 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -6,7 +6,7 @@ jsproxy_config({
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
-    enable: false,
+    enable: true,
     ver: 62
   },
 
@@ -35,10 +35,10 @@ jsproxy_config({
       label: '',
       hidden: true,
       lines: {
-        // 收费版（高权重）
+        //收费版（高权重）
         //'node-cfworker-2.etherdream.com': 4,
 
-        // 免费版（低权重，分摊一些成本）
+           //免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
         //'b.007.workers.dev': 1,
         //'b.hehe.workers.dev': 1,
@@ -63,10 +63,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-//assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-   assets_cdn: 'assets/',
+//   assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
