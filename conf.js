1c44132da205b40418ecb6bf03a3fa5fdc8f0536
YidaozhanYa <73830635+YidaozhanYa@users.noreply.github.com>
2020-11-03 20:15:04 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -6,16 +6,16 @@ jsproxy_config({
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
-    enable: true,
+    enable: false,
     ver: 62
   },
 
   // 节点配置
   node_map: {
     'demo-hk': {
-      label: 'Bulink加速',
+      label: '演示服务-香港节点',
       lines: {
-        'js.bulink.xyz': 1,
+        'node-aliyun-sg.etherdream.com:8443': 1,
       },
     },
     'demo-sg': {
@@ -63,10 +63,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+//assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  //   assets_cdn: 'assets/',
+   assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
